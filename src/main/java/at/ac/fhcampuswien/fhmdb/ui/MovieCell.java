2e10b40b9a5632a587a8d138526f97dd87568437
ArAs101 <armin.assadzadeh@stud.fh-campuswien.ac.at>
2023-04-10 21:47:37 +0200

    Made display of Release year and Rating visible in the movie results list

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -8,13 +8,16 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+
 import java.util.stream.Collectors;
 
 public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
     private final Label genre = new Label();
-    private final VBox layout = new VBox(title, detail, genre);
+    private final Label releaseYear = new Label();
+    private final Label rating = new Label();
+    private final VBox layout = new VBox(title, detail, genre, releaseYear, rating);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -37,7 +40,7 @@ public class MovieCell extends ListCell<Movie> {
                     .map(Enum::toString)
                     .collect(Collectors.joining(", "));
             genre.setText(genres);
-
+            //genre.getStyleClass().add("text-genre");
 
             // color scheme
             title.getStyleClass().add("text-yellow");
@@ -45,6 +48,10 @@ public class MovieCell extends ListCell<Movie> {
             genre.getStyleClass().add("text-white");
             genre.setStyle("-fx-font-style: italic");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
+            releaseYear.setText("Released: " + movie.getReleaseYear());
+            releaseYear.getStyleClass().add("text-white");
+            rating.setText("Rating: " + movie.getRating() + " / 10");
+            rating.getStyleClass().add("text-white");
 
             // layout
             title.fontProperty().set(title.getFont().font(20));
